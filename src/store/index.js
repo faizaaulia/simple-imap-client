e53b1e8267d318b45013d6fcb08adda3dc424376
faizaaulia <faizaaulia.id@gmail.com>
2021-03-19 21:15:01 +0700

    mails getters

--- src/store/index.js
+++ src/store/index.js
@@ -7,20 +7,26 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     loggedIn: false,
-    user: null
+    user: null,
+    mails: null
   },
   mutations: {
     login(state, user) {
-      state.loggedIn = true
-      state.user = user
       axios.post('http://localhost:3000/mail', user).then(res => {
-        console.log(res)
+        state.loggedIn = true
+        state.user = user
+        state.mails = res.data
       })
     }
   },
   actions: {
     login({ commit }, user) {
       if (user.email && user.email) commit('login', user)
+    }
+  },
+  getters: {
+    getMails(state) {
+      return state.mails
     }
   },
   modules: {
--- src/views/Inbox.vue
+++ src/views/Inbox.vue
@@ -2,9 +2,9 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div class="home">
     <v-container>
-      <v-row justify="center">
+      <v-row justify="center" v-if="! $store.state.loggedIn">
         <v-col cols="6">
-          <v-card v-if="! $store.state.loggedIn" class="mt-5">
+          <v-card class="mt-5">
             <v-card-title primary-title>
               Login
             </v-card-title>
@@ -40,7 +40,28 @@
               </v-row>
             </v-container>
           </v-card>
-          <h3 v-else>Inbox</h3>
+        </v-col>
+      </v-row>
+      <v-row v-else class="my-5">
+        <v-col
+          cols="12"
+        >
+          <v-card>
+            <v-subheader>Inbox</v-subheader>
+            <v-divider></v-divider>
+            <v-list two-line>
+              <template v-for="(mail, index) in mails">
+                <v-list-item :key="index">
+                  <v-list-item-avatar color="grey darken-1"></v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title>{{ mail.subject }}</v-list-item-title>
+                    <v-list-item-subtitle>from {{ mail.from }}</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider inset :key="`div${index}`" v-if="`div${index}` !== `div${mails.length-1}`"></v-divider>
+              </template>
+            </v-list>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
@@ -50,13 +71,14 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Inbox',
   data: () => ({
     user: {
-        email: null,
-        password: null
+      email: null,
+      password: null
     }
   }),
   methods: {
@@ -64,6 +86,11 @@ export default {
       this.$refs.form.validate()
     },
   },
+  computed: {
+    ...mapGetters({
+      mails: 'getMails'
+    })
+  }
   // components: {
   //   HelloWorld
   // }
